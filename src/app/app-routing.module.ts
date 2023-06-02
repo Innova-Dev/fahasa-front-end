db33f95d55f44b34949bebdedff14ddcd3f856c7
rynddev <manhnvph25559@fpt.edu.vn>
2023-06-02 22:48:10 +0700

    feat:giao dien chi tiet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout/layout.component';
 import { ProductComponent } from './pages/product/product.component';
-import { DetailProductComponent } from './pages/detail-product/detail-product.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 const routes: Routes = [
   { path: '', component: LayoutComponent, children: [
     { path: '', component: ProductComponent},
-    { path: 'product-detail', component: DetailProductComponent},
+    { path: 'product-detail', component: ProductDetailComponent},
     { path: 'cart', component: CartComponent},
     { path: 'checkout', component: CheckoutComponent},
   ]}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout/layout.component';
@@ -8,22 +8,24 @@ import { LayoutComponent } from './layout/layout/layout.component';
 // import { DetailProductComponent } from './pages/detail-product/detail-product/detail-product.component';
 // import { CartComponent } from './pages/carts/cart/cart.component';
 import { ProductComponent } from './pages/product/product.component';
-import { DetailProductComponent } from './pages/detail-product/detail-product.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
     ProductComponent,
-    DetailProductComponent,
+    ProductDetailComponent,
     CartComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
