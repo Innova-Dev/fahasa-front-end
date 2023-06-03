b443008d5b6b41b50f24ab487c2e6cdd92cb6ead
Hop Duong <hop281003@gmail.com>
2023-06-03 09:25:16 +0700

    feat:giao dien dangky dang nhap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductComponent } from './pages/product/product.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ProductDetailComponent } from './pages/product-detail/product-detail.co
     ProductDetailComponent,
     CartComponent,
     CheckoutComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
