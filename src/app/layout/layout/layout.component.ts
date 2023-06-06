007abfdd7698ffa2d48e7f448cce944a6b40f556
rynddev <manhnvph25559@fpt.edu.vn>
2023-06-06 23:45:36 +0700

    feat: add to cart localstrogare refs:SAH-36

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
+  cartItemCount: number = 0;
 
+  constructor(private cartService: CartService) {
+    this.updateCartItemCount();
+  }
+
+  updateCartItemCount(): void {
+    const cartItems = this.cartService.getCartItems();
+    this.cartItemCount = cartItems.length;
+  }
+  ngOnInit(): void {
+    this.cartService.getCartItemCount().subscribe((count: number) => {
+      this.cartItemCount = count;
+    });
+  }
 }
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private cartItemCount: number = 0;
+  private cartItemCountSubject: BehaviorSubject<number> = new BehaviorSubject<number>(this.cartItemCount);
+  private cartItems: any[] = [];
+
+  constructor() {
+    // Kiểm tra xem có dữ liệu giỏ hàng trong Local Storage hay không
+    const storedCartItems = localStorage.getItem('cartItems');
+    if (storedCartItems) {
+      this.cartItems = JSON.parse(storedCartItems);
+      this.cartItemCount = this.cartItems.length
+      this.cartItemCountSubject = new BehaviorSubject<number>(this.cartItems.length);
+    }
+  }
+
+  getCartItems(): any[] {
+    return this.cartItems;
+  }
+
+  getCartItemCount(): BehaviorSubject<number> {
+    return this.cartItemCountSubject;
+  }
+
+  addToCart(item: any): void {
+    this.cartItems.push(item);
+    this.cartItemCount++;
+    this.cartItemCountSubject.next(this.cartItemCount);
+    this.updateLocalStorage();
+  }
+
+  
+
+  private updateLocalStorage(): void {
+    localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
+  }
+}
