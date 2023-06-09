491fced0712f02b524946f98f8f3706d43f00529
rynddev <manhnvph25559@fpt.edu.vn>
2023-06-09 10:22:02 +0700

    feat: them san pham refs:SAH-5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { LayoutAdminComponent } from './layout/LayoutAdmin/layout-admin/layout-a
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 import { ProductListComponent } from './pages/admin/products/product-list/product-list.component';
 import { ProductEditComponent } from './pages/admin/products/product-edit/product-edit.component';
+import { ProductAddComponent } from './pages/admin/products/product-add/product-add.component';
 const routes: Routes = [
   { path: '', component: LayoutComponent, children: [
     { path: '', component: ProductComponent},
@@ -28,6 +29,7 @@ const routes: Routes = [
     {path: 'categories/edit', component: EditCateComponent},
     {path:'products',children:[
       {path:'',component:ProductListComponent},
+      {path:'add',component:ProductAddComponent},
       {path:'edit/:_id',component:ProductEditComponent}
     ]}
   ]
--- src/app/pages/admin/products/product-add/product-add.component.ts
+++ src/app/pages/admin/products/product-add/product-add.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
-
+import { ProductService } from 'src/app/services/product.service';
+import { FormBuilder } from '@angular/forms';
+import { IProduct } from 'src/app/interfaces/product';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent {
+  constructor (
+    private formBuilder: FormBuilder,
+    private productService: ProductService,  private router: Router
+  ) {}
 
+  productForm = this.formBuilder.group({
+    name: [''],
+    list_price: [0],
+    original_price: [0],
+    images: [[]], // Khởi tạo images với một mảng rỗng
+    description: [''],
+  });
+
+  onHandleSubmit() {
+    if (this.productForm.valid) {
+      const product: IProduct = {
+        name: this.productForm.value.name || '',
+        list_price: this.productForm.value.list_price || 0,
+        original_price: this.productForm.value.original_price || 0,
+        images: Array.isArray(this.productForm.value.images) ? this.productForm.value.images : [], // Chuyển đổi images thành mảng nếu không phải là mảng
+        description: this.productForm.value.description || '',
+      };
+
+      this.productService.addProduct(product).subscribe(data => {
+        console.log(data);
+        console.log(this.productForm.value.images);
+        alert("Thêm sản phẩm thành công!")
+        this.router.navigate(['/admin/products']);
+      });
+    }
+  }
 }
